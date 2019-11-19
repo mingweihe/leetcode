db86f755aabf72c1a03b5bd93800125bc4aea110
Mingwei He <hemingwei2017@gmail.com>
2019-11-18 20:34:04 -0800

    1258 solution update

--- _1258_Synonymous_Sentences.py
+++ _1258_Synonymous_Sentences.py
@@ -7,30 +7,36 @@ class Solution(object):
         :type synonyms: List[List[str]]
         :type text: str
         :rtype: List[str]
+            union find / disjoint set + backtracking
         """
-        dic = collections.defaultdict(set)
-        for x, y in synonyms:
-            if x in dic or y in dic:
-                sets = dic[x] or dic[y]
-                sets.add(y)
-                sets.add(x)
-                dic[x] = dic[y] = sets
-            else: dic[x] = dic[y] = set([x, y])
+        dic = collections.defaultdict(str)
+        for x, y in synonyms: dic[x], dic[y] = x, y
 
-        def bt(cur, pos):
+        def find(key):
+            if key != dic[key]:
+                dic[key] = find(dic[key])
+            return dic[key]
+        for x, y in synonyms:
+            x, y = find(x), find(y)
+            if x == y: continue
+            dic[x] = y
+        graphs = collections.defaultdict(set)
+        for k in dic: graphs[find(k)].add(k)
+
+        def bt(cur, pos, ans):
             if pos == len(text):
-                self.res += ' '.join(cur),
+                ans += ' '.join(cur),
                 return
             if text[pos] not in dic:
                 cur += text[pos],
-                bt(cur, pos+1)
+                bt(cur, pos+1, ans)
                 cur.pop(),
                 return
-            for w in dic[text[pos]]:
+            for w in graphs[dic[text[pos]]]:
                 cur += w,
-                bt(cur, pos+1)
+                bt(cur, pos+1, ans)
                 cur.pop()
-        self.res = []
+        res = []
         text = text.split()
-        bt([], 0)
-        return sorted(self.res)
+        bt([], 0, res)
+        return sorted(res)
